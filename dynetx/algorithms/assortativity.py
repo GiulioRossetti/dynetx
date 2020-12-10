b5f06551409f33468fb673b31b4b083b3677a458
Giulio Rossetti <giulio.rossetti@gmail.com>
2020-12-10 15:30:04 +0100

    :bug: fixed some slicing issues

--- dynetx/algorithms/assortativity.py
+++ dynetx/algorithms/assortativity.py
@@ -133,8 +133,10 @@ def delta_conformity(dg, start: int, delta: int, alphas: list, labels: list, pro
 
     res = {str(a): {"_".join(profile): {n: 0 for n in g.nodes()} for profile in profiles} for a in alphas}
 
-    mid = max(g.temporal_snapshots_ids())
-    sp = all_time_respecting_paths(g, start, min(mid, delta + start))
+    tids = g.temporal_snapshots_ids()
+    mid = max(tids)
+    mmid = min(tids)
+    sp = all_time_respecting_paths(g, max(start, mmid), min(mid, delta + start))
 
     distances = defaultdict(lambda: defaultdict(int))
     for k, v in list(sp.items()):
