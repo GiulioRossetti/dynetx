8ed4147e562362bcdd2c363327949646f94072fd
Giulio Rossetti <giulio.rossetti@gmail.com>
2021-06-24 08:47:04 +0200

    :arrow_up: actions

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup, find_packages
+from codecs import open
+from os import path
 
 __author__ = 'Giulio Rossetti'
 __license__ = "BSD"
@@ -8,14 +10,23 @@ __email__ = "giulio.rossetti@gmail.com"
 # with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 #    long_description = f.read()
 
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    requirements = f.read().splitlines()
+
+
 setup(name='dynetx',
-      version='0.2.5',
+      version='0.2.6',
       license='BSD-Clause-2',
       description='Dynamic Network library',
       url='https://github.com/GiulioRossetti/dynetx',
       author='Giulio Rossetti',
       author_email='giulio.rossetti@gmail.com',
-      use_2to3=True,
       classifiers=[
           # How mature is this project? Common values are
           #   3 - Alpha
@@ -38,6 +49,8 @@ setup(name='dynetx',
           'Programming Language :: Python :: 3'
       ],
       keywords='dynamic-networks',
-      install_requires=['numpy', 'networkx', 'scipy', 'tqdm', ''],
+      install_requires=requirements,
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       packages=find_packages(exclude=["*.test", "*.test.*", "test.*", "test", "dynetx.test", "dynetx.test.*"]),
       )
